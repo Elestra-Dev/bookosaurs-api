2e9071965eec351034a969ff5eb89905be338cd1
Amit Roy <amit1roy919@gmail.com>
2025-08-05 13:13:22 +0530

    bujhli

--- src/server.ts
+++ src/server.ts
@@ -13,5 +13,5 @@ app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at -Miau  ${PORT}`);
+  console.log(`🚀 Server running at -Miau koris na besi besi ${PORT}`);
 });
